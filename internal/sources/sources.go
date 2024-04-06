c97d6bda947c1ee67a639e30746a95c128947ccc
@bellamariz <isabellafmariz@gmail.com>
2024-04-06 19:50:01 -0300

    perf: Create method in package sources to verify if ingest is active

--- internal/sources/sources.go
+++ internal/sources/sources.go
@@ -1,5 +1,11 @@
 package sources
 
+import (
+	"time"
+
+	"github.com/rs/zerolog/log"
+)
+
 type Ingest struct {
 	Signal       string   `json:"signal"`
 	Packagers    []string `json:"packagers"`
@@ -9,4 +15,17 @@ type Ingest struct {
 type Source struct {
 	Signal string `json:"signal"`
 	Server string `json:"server"`
+}
+
+func (i Ingest) IsActive() bool {
+	t, err := time.Parse(time.RFC1123, i.LastReported)
+	if err != nil {
+		log.Warn().Err(err).Msg("Error when verifying if ingest is active")
+		return false
+	}
+
+	wasRecentlyReported := time.Since(t) <= (15 * time.Second)
+	hasActiveIngest := len(i.Packagers) > 0
+
+	return wasRecentlyReported && hasActiveIngest
 }
